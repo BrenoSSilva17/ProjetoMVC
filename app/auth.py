152b23eb59c9ca6fef08f8871a2896987bf15d9d
Breno <brenaosilva881@gmail.com>
2026-05-15 13:51:56 -0300

    Funções de autenticação

---
+++ app/auth.py
@@ -0,0 +1,79 @@
+# Logica de autenticação
+
+# 1. Hash e verificação de senhas com bcrypt
+
+# 2. Geração de token JWT
+
+# 3. Leitura e validação do token vindo do cookie
+
+from datetime import datetime, timedelta, timezone
+from jose import JWSError, jwt 
+from passlib.context import CryptContext
+from fastapi import Request, HTTPException, status
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+# Configurar o algoritmo de hash = bcrypt
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Função de senha
+
+def hash_senha(senha: str):
+    return pwd_context.hash(senha)
+
+def verificar_senha(senha: str, senha_hash: str):
+    return pwd_context.verify(senha, senha_hash)
+
+#Função do token JWT
+
+def criar_token(dados: dict):
+
+    payload = dados.copy()
+
+    #Define quando o token expira
+    expira = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload.update({"exp": expira})
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return token
+
+def decodificar_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return payload
+  
+
+# Função para usar nas rotas protegidas
+def obter_usuario_logado(request: Request):
+
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Não autenticado"
+        )
+    
+    try:
+        payload = decodificar_token(token)
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido"
+        )
+
+        return payload
+    except JWSError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido ou expirado"
+        )
+    
